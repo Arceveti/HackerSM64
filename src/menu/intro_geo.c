f5cce87d6cdace4f4b8976755b8421ecb961caac
Arceveti <arceveti@gmail.com>
2022-07-19 10:02:02 -0700

    Fix ENABLE_RUMBLE ifdefs

--- src/menu/intro_geo.c
+++ src/menu/intro_geo.c
@@ -412,7 +412,7 @@ Gfx *geo_intro_face_easter_egg(s32 callContext, struct GraphNode *node, UNUSED v
 }
 #endif
 
-#if defined(ENABLE_RUMBLE)
+#if ENABLE_RUMBLE
 extern Gfx title_screen_bg_dl_rumble_pak[];
 
 Gfx *geo_intro_rumble_pak_graphic(s32 callContext, struct GraphNode *node, UNUSED void *context) {
