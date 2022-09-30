ef696fa6ec4b6490fb4b4562be965cf3a2f80b0b
Arceveti <arceveti@gmail.com>
2022-09-30 16:08:54 -0700

    Add vec3f_normalize_check + fix read_surface_data crash

--- src/engine/surface_load.c
+++ src/engine/surface_load.c
@@ -255,7 +255,7 @@ static struct Surface *read_surface_data(TerrainData *vertexData, TerrainData **
 
     find_vector_perpendicular_to_plane(n, v[0], v[1], v[2]);
 
-    vec3f_normalize(n);
+    if (!vec3f_normalize_check(n)) return NULL;
 
     struct Surface *surface = alloc_surface(dynamic);
 
