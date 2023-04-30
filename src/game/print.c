89bf58fbc6bdffdc0199f39be70f4019772066c7
Arceveti <arceveti@gmail.com>
2023-04-30 19:16:49 -0400

    Fix build

--- src/game/print.c
+++ src/game/print.c
@@ -50,7 +50,7 @@ void format_integer(s32 n, s32 base, char* dest, s32* totalLength, u8 width, _Bo
     s32 i;
     s32 len = 0;
     s8 digit;
-    _Bool negative = FALSE;
+    _Bool isNegative = FALSE;
     char pad;
 
     if (zeroPad) {
@@ -63,7 +63,7 @@ void format_integer(s32 n, s32 base, char* dest, s32* totalLength, u8 width, _Bo
         // Formats a negative number for negative prefix.
         if (n < 0) {
             n = -n;
-            negative = TRUE;
+            isNegative = TRUE;
         }
 
         // Increments the number of digits until length is long enough.
@@ -79,31 +79,31 @@ void format_integer(s32 n, s32 base, char* dest, s32* totalLength, u8 width, _Bo
 
         // Add leading pad to fit width.
         if (width > numDigits) {
-            for (len = 0; len < width - numDigits; len++) {
+            for (len = 0; len < (width - numDigits); len++) {
                 dest[len] = pad;
             }
 
             // Needs 1 length to print negative prefix.
-            if (negative) {
+            if (isNegative) {
                 len--;
             }
         }
 
         // Use 'M' prefix to indicate negative numbers.
-        if (negative) {
+        if (isNegative) {
             dest[len] = 'M';
             len++;
         }
 
         // Transfer the digits into the proper base.
-        for (i = numDigits - 1; i >= 0; i--) {
+        for (i = (numDigits - 1); i >= 0; i--) {
             powBase = int_pow(base, i);
-            digit = n / powBase;
+            digit = (n / powBase);
 
             if (digit < 10) {
-                dest[len + numDigits - 1 - i] = digit + '0';
+                dest[((len + numDigits) - 1) - i] = (digit + '0');
             } else {
-                dest[len + numDigits - 1 - i] = digit + '7';
+                dest[((len + numDigits) - 1) - i] = (digit + '7');
             }
 
             n -= digit * powBase;
@@ -111,7 +111,9 @@ void format_integer(s32 n, s32 base, char* dest, s32* totalLength, u8 width, _Bo
     } else { // n is zero.
         numDigits = 1;
         if (width > numDigits) {
-            for (len = 0; len < width - numDigits; len++) dest[len] = pad;
+            for (len = 0; len < (width - numDigits); len++) {
+                dest[len] = pad;
+            }
         }
         dest[len] = '0';
     }
@@ -136,10 +138,10 @@ void parse_width_field(const char* str, s32* srcIndex, u8* width, _Bool* zeroPad
 
     // Read width digits up until the 'd' or 'x' format specifier.
     while (
-        str[*srcIndex] != 'b' &&
-        str[*srcIndex] != 'o' &&
-        str[*srcIndex] != 'd' &&
-        str[*srcIndex] != 'x'
+        (str[*srcIndex] != 'b') &&
+        (str[*srcIndex] != 'o') &&
+        (str[*srcIndex] != 'd') &&
+        (str[*srcIndex] != 'x')
     ) {
         digits[digitsLen] = str[*srcIndex] - '0';
 
@@ -158,7 +160,7 @@ void parse_width_field(const char* str, s32* srcIndex, u8* width, _Bool* zeroPad
     }
 
     // Sum the digits to calculate the total width.
-    for (i = 0; i < digitsLen - 1; i++) {
+    for (i = 0; i < (digitsLen - 1); i++) {
         *width += (digits[i] * (((digitsLen - i) - 1) * 10));
     }
 
@@ -173,7 +175,7 @@ void parse_width_field(const char* str, s32* srcIndex, u8* width, _Bool* zeroPad
  * related to overflow. For romhacks, prefer sprintf + print_text.
  */
 void print_text_fmt_int(s32 x, s32 y, const char* str, s32 n) {
-    char c = 0;
+    char c = '\0';
     _Bool zeroPad = FALSE;
     u8 width = 0;
     s32 base = 0;
@@ -181,8 +183,7 @@ void print_text_fmt_int(s32 x, s32 y, const char* str, s32 n) {
     s32 srcIndex = 0;
 
     // Don't continue if there is no memory to do so.
-    if ((sTextLabels[sTextLabelsCount] = mem_pool_alloc(gEffectsMemoryPool,
-                                                        sizeof(struct TextLabel))) == NULL) {
+    if ((sTextLabels[sTextLabelsCount] = mem_pool_alloc(gEffectsMemoryPool, sizeof(struct TextLabel))) == NULL) {
         return;
     }
 
@@ -191,17 +192,17 @@ void print_text_fmt_int(s32 x, s32 y, const char* str, s32 n) {
 
     c = str[srcIndex];
 
-    while (c != 0) {
+    while (c != '\0') {
         if (c == '%') {
             srcIndex++;
 
             parse_width_field(str, &srcIndex, &width, &zeroPad);
 
             if (
-                str[srcIndex] != 'b' &&
-                str[srcIndex] != 'o' &&
-                str[srcIndex] != 'd' &&
-                str[srcIndex] != 'x'
+                (str[srcIndex] != 'b') &&
+                (str[srcIndex] != 'o') &&
+                (str[srcIndex] != 'd') &&
+                (str[srcIndex] != 'x')
             ) {
                 break;
             }
@@ -212,7 +213,7 @@ void print_text_fmt_int(s32 x, s32 y, const char* str, s32 n) {
 
             srcIndex++;
 
-            format_integer(n, base, sTextLabels[sTextLabelsCount]->buffer + len, &len, width, zeroPad);
+            format_integer(n, base, (sTextLabels[sTextLabelsCount]->buffer + len), &len, width, zeroPad);
         } else { // straight copy
             sTextLabels[sTextLabelsCount]->buffer[len] = c;
             len++;
@@ -229,7 +230,7 @@ void print_text_fmt_int(s32 x, s32 y, const char* str, s32 n) {
  * Prints text in the colorful lettering at given X, Y coordinates.
  */
 void print_text(s32 x, s32 y, const char* str) {
-    char c = 0;
+    char c = '\0';
     s32 length = 0;
     s32 srcIndex = 0;
 
@@ -259,7 +260,7 @@ void print_text(s32 x, s32 y, const char* str) {
  * Prints text in the colorful lettering centered at given X, Y coordinates.
  */
 void print_text_centered(s32 x, s32 y, const char* str) {
-    char c = 0;
+    char c = '\0';
     s32 length = 0;
     s32 srcIndex = 0;
 
@@ -271,7 +272,7 @@ void print_text_centered(s32 x, s32 y, const char* str) {
     c = str[srcIndex];
 
     // Set the array with the text to print while finding length.
-    while (c != 0) {
+    while (c != '\0') {
         sTextLabels[sTextLabelsCount]->buffer[length] = c;
         length++;
         srcIndex++;
@@ -350,8 +351,13 @@ void render_textrect(s32 x, s32 y, s32 pos) {
 #endif
     rectX = rectBaseX;
     rectY = rectBaseY;
-    gSPTextureRectangle(gDisplayListHead++, (rectX << 2), (rectY << 2), ((rectX + 15) << 2),
-                        ((rectY + 15) << 2), G_TX_RENDERTILE, 0, 0, (4 << 10), (1 << 10));
+    gSPTextureRectangle(gDisplayListHead++,
+        (rectX << 2), (rectY << 2),
+        ((rectX + 15) << 2), ((rectY + 15) << 2),
+        G_TX_RENDERTILE,
+        0, 0,
+        (4 << 10), (1 << 10)
+    );
 }
 
 /**
